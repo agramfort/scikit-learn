f0c51a0f9bcf2352255086f649b78c8795ed6b34
Matthieu Brucher <matthieu.brucher@gmail.com>
2010-07-21 23:42:12 +0200

    Deleted old code
    Starting tests for the geodesic_mds module

--- scikits/learn/manifold/embedding/tests/test_distances.py
+++ scikits/learn/manifold/embedding/tests/test_distances.py
@@ -4,37 +4,7 @@ import unittest
 import numpy
 
 from unittest import TestCase
-from ..distances import parzen, kneigh, numpy_floyd
-
-class TestParzen(TestCase):
-  def test_main(self):
-    samples = numpy.array((0., 0., 0.,
-      1., 0., 0.,
-      0., 1., 0.,
-      1., 1., 0.,
-      0., .5, 0.,
-      .5, 0., 0.,
-      1., 1., 0.5,
-      )).reshape((-1,3))
-    neighbors = parzen(samples, .75)
-    for i, neighlist in enumerate(neighbors):
-      for neighbor in neighlist:
-        d = samples[i] - samples[neighbor]
-        assert( numpy.sum(d**2) < .75**2 )
-
-class TestKNeigh(TestCase):
-  def test_main(self):
-    samples = numpy.array((0., 0., 0.,
-      1., 0., 0.,
-      0., 1., 0.,
-      1., 1., 0.,
-      0., .5, 0.,
-      .5, 0., 0.,
-      1., 1., 0.5,
-      )).reshape((-1,3))
-    neighbors = kneigh(samples, 2)
-    for i, neighlist in enumerate(neighbors):
-      assert(len(neighlist) == 2)
+from ..distances import numpy_floyd
 
 class TestNumpyFloyd(TestCase):
   pass
---
+++ scikits/learn/manifold/embedding/tests/test_geodesic_mds.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import unittest
+import numpy
+from numpy.testing import assert_array_equal, \
+                          assert_array_almost_equal, \
+                          assert_raises
+
+from unittest import TestCase
+from ....neighbors import Neighbors
+from ..geodesic_mds import populate_distance_matrix_from_neighbors
+
+class TestPopulateMatrix(TestCase):
+  def test_main(self):
+    samples = numpy.array((0., 0., 0.,
+      1., 0., 0.,
+      0., 1., 0.,
+      1., 1., 0.,
+      0., .5, 0.,
+      .5, 0., 0.,
+      1., 1., 0.5,
+      )).reshape((-1,3))
+    neigh = Neighbors(k=3)
+    neigh.fit(samples)
+    neigh = neigh.kneighbors
+    dists = populate_distance_matrix_from_neighbors(samples, neigh)
+    assert_array_equal(dists, dists.T)
+    print dists
+
+if __name__ == "__main__":
+  unittest.main()
+  
