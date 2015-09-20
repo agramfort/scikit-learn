6854276c78b75994ad2e36d3a0ac459deefc15ee
MechCoder <manojkumarsivaraj334@gmail.com>
2015-09-20 19:45:42 -0400

    Add gradient calculation in _huber_loss_and_gradient

--- sklearn/linear_model/huber.py
+++ sklearn/linear_model/huber.py
@@ -18,24 +18,30 @@ def _huber_loss_and_gradient(w, X, y, epsilon, alpha):
     loss = 0.5 * np.dot(non_outliers, non_outliers) + outlier_loss
 
     # Calulate the gradient
-    grad = np.zeros(w.shape[0])
-
-    return loss + alpha * np.dot(w, w)
+    grad = np.dot(non_outliers, -X[~outliers_true, :])
+    outliers_true_pos = np.logical_and(linear_loss >= 0, outliers_true)
+    outliers_true_neg = np.logical_and(linear_loss < 0, outliers_true)
+    grad -= epsilon * X[outliers_true_pos, :].sum(axis=0)
+    grad += epsilon * X[outliers_true_neg, :].sum(axis=0)
+    grad += alpha * 2 * w
+    return loss + alpha * np.dot(w, w), grad
 
 
 class HuberRegressor(RegressorMixin, BaseEstimator):
-    def __init__(self, epsilon=0.1, n_iter=100, alpha=0.0001):
+    def __init__(self, epsilon=0.1, n_iter=100, alpha=0.0001, warm_start=False):
         self.epsilon = epsilon
         self.n_iter = n_iter
         self.alpha = alpha
+        self.warm_start = warm_start
 
     def fit(self, X, y):
         X, y = check_X_y(X, y)
 
-        coef = np.zeros(X.shape[0])
-        coef, f, _ = optimize.fmin_l_bfgs_b(
-        	_huber_loss_and_gradient, coef, approx_grad=True,
+        coef = getattr(self, 'coef', None)
+        if not self.warm_start or (self.warm_start and coef is None):
+            self.coef_ = np.zeros(X.shape[1])
+        self.coef, f, _ = optimize.fmin_l_bfgs_b(
+            _huber_loss_and_gradient, self.coef_,
         	args=(X, y, self.epsilon, self.alpha), maxiter=self.n_iter)
-        self.coef_ = coef
-        self.f_ = f
+        self.loss_ = f
         return self
